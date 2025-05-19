b0f38048154205986b6455651785df15168d655f
agnestiw <agnestiwulansari605@gmail.com>
2025-05-19 20:26:44 +0700

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Catalog from "./Pages/Catalog";
 import Wishlist from "./Pages/Wishlist";
-import 
+import Article from "./Pages/Article";
+import About from "./Pages/About";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
